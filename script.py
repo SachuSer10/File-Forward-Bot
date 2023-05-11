cbd13e6cdf746307a8370e0183f30ffbd067fc33
SachuSer10 <108418648+SachuSer10@users.noreply.github.com>
2023-05-11 08:41:58 +0530

    Update script.py

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ class scripts(object):
 
     START_TXT = """
 <b>Hᴇʟʟᴏ {},
-Mʏ Nᴀᴍᴇ Is <a href=https://t.me/{}>{}</a>, I'm Just A Private Forward Bot Made By <a href='t.me/creatorbeatz'>Jᴏᴇʟ ᠰ TɢX</a>.</b>"""
+Mʏ Nᴀᴍᴇ Is <a href=https://t.me/{}>{}</a>, I'm Just A Private Forward Bot</a>.</b>"""
 
     ABOUT_TXT = """
 <b>✯ Mʏ Nᴀᴍᴇ: {}
